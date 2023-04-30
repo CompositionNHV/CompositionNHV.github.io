4ec50ce7a7be8306f2ca33bf1e6fe7caf761057a
Estelle GUEVILLE <82150756+EstelleGvl@users.noreply.github.com>
2023-04-29 22:07:13 -0400

    image

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -42,22 +42,3 @@ var cbpAnimatedHeader = (function() {
 	init();
 
 })();
-
-<script>
-// Get the video
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
-</script>
